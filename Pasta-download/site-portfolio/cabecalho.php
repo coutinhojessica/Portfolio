883d4035db96ea3b413fa118c8ed4f670542df45
Jessica Coutinho <jessicamaiara.coutinho@gmail.com>
2022-04-20 17:07:49 -0300

    Melhorando o Site

--- Pasta-download/site-portfolio/cabecalho.php
+++ Pasta-download/site-portfolio/cabecalho.php
@@ -1,36 +1,41 @@
 <!DOCTYPE html>
-<html lang="PT_BR">
+<html lang="PT-BR">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/CSS/style-page.css">
-    <title>Cabeçalho Páginas</title>
-    <link rel="shortcut icon" href="assets/Imagens/favicon_io2/favicon.ico" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" href="style.css">
+    <title>Cabeçalho do site</title>
 </head>
 <body>
-    <div class="conteiner-cabecalho inline-block">
-        <header>
-            <div class="cabecalho inline-block">
-                <h1 class="titulo-page">Jéssica Coutinho</h1>
+    <header>
+        <nav class="nav-cabecalho navbar navbar-expand-sm navbar-light p-3 pt-3 ">
+            <div class="container-fluid">
+              <a class="logo navbar-brand disabled">Jéssica Coutinho</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
+                <ul class="navbar-nav menu">
+                  <li class="nav-item">
+                    <a class="nav-link" href="sobre.php">SOBRE MIM</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="projetos.php">PROJETOS</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="trabalho.php">TRABALHOS</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="contato.php">CONTATO</a>
+                  </li>
+                </ul>
+              </div>
             </div>
-                <div class="conteiner-menu inline-block">
-                    <nav class="menu">  
-                        <ul class="menu-page">
-                            <li><a href="index.html">Início</a></li>
-                            <li><a href="sobre.php">Sobre Mim</a></li>
-                            <li><a href="projetos.php">Projetos</a></li>
-                            <li><a href="trabalhos.php">Trabalhos</a></li>
-                            <li><a href="contato.php">Contato</a></li>
-                        </ul>
-                    </nav>
-                </div>
-        </header>
-    </div>
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+          </nav>
+    </header>
 </body>
 </html>
